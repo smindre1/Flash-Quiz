df6c10cb605beb4e3becaa8703e5ecebd55dab4a
Shane Mindreau <smindre1@binghamton.edu>
2024-11-16 15:19:14 -0500

    *Patch: I forgot to add the endpoint '/api' to the DB_URL value in the environment.prod.ts file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    DB_URL: "https://flash-quiz-db-38f7a56d8726.herokuapp.com/",
+    DB_URL: "https://flash-quiz-db-38f7a56d8726.herokuapp.com/api/",
 }
